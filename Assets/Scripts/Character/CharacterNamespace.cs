98ddb44514056f09f5ac1d21eb762ce4c3fa5c80
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-08-17 15:43:45 +0900

    Add values

--- Assets/Scripts/Character/CharacterNamespace.cs
+++ Assets/Scripts/Character/CharacterNamespace.cs
@@ -7,11 +7,15 @@ namespace CharacterNamespace
         public int Level;
         public int GrowGage;
         public int Money;
+        public int Temper;
+        public float Water;
         public CharacterStatus()
         {
             Level = 0;
             GrowGage = 0;
             Money = 2000;
+            Temper = 25;
+            Water = 0.5f;
         }
     }
 }
--- Assets/Scripts/FIxedValue.cs
+++ Assets/Scripts/FIxedValue.cs
@@ -6,4 +6,5 @@ public static class FixedValue
     public static string SAVE_PATH = $"{Application.persistentDataPath}/{"gamedata.save"}";
     public static int MAX_LEVEL = 4;
     public static List<int> MAX_GAGE = new List<int>() { 500, 3000, 10000, 100000, 200000 };
+    public static List<string> LEVEL_NAME = new List<string>() { "초여름", "한여름", "늦여름" };
 }
