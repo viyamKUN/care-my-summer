ba4b2811a4a548022780cb0185b6ec328343fbab
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-08-17 17:09:34 +0900

    Add CSV reader and csv file

---
+++ Assets/Scripts/CSVReader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVReader : MonoBehaviour
+{
+    [SerializeField] private TextAsset _environment = null;
+    [SerializeField] private TextAsset _items = null;
+    public void ReadEnvironments()
+    {
+
+    }
+    public void ReadItems()
+    {
+
+    }
+}
