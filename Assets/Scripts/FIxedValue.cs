543ae8441ce5b2be2f69eac4c37939e113b4e127
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-08-18 08:40:40 +0900

    Update FIxedValue.cs

--- Assets/Scripts/FIxedValue.cs
+++ Assets/Scripts/FIxedValue.cs
@@ -5,7 +5,7 @@ public static class FixedValue
 {
     public static string SAVE_PATH = $"{Application.persistentDataPath}/{"gamedata.save"}";
     public static int MAX_LEVEL = 4;
-    public static List<int> MAX_GAGE = new List<int>() { 500, 3000, 10000, 20000, 50000 };
+    public static List<int> MAX_GAGE = new List<int>() { 500, 3000, 7000, 13000, 20000 };
     public static List<string> LEVEL_NAME = new List<string>() { "편여름", "초여름", "한여름", "늦여름", "완여름" };
     public static int MAX_TEMPER = 40;
     public static Dictionary<SceneName, string> SCENE_NAME = new Dictionary<SceneName, string>() {
