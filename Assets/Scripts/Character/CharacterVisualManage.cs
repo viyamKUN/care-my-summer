88ab67e854c059759c46f8394d7c94d3babbc6e2
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-08-17 15:14:13 +0900

    Add Animation Controller
    * Use Blend tree
    * Use layers

--- Assets/Scripts/Character/CharacterVisualManage.cs
+++ Assets/Scripts/Character/CharacterVisualManage.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class CharacterVisualManage : MonoBehaviour
 {
     [SerializeField] private Animator _anim = null;
+    [SerializeField] private Animator _faceAnim = null;
     int _thisLev = 0;
 
     public void SetLevel(uint level)
     {
         this._thisLev = (int)level;
         _anim.SetFloat("level", level * 0.25f);
+        _faceAnim.SetLayerWeight((int)level, 1);
+        _faceAnim.SetFloat("temper", 0);
+        _faceAnim.SetFloat("water", 0);
     }
 }
