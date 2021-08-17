5cb372a4e93cabddfc83a4ff61ccefd7f943056c
뱀 <54323618+viyamKUN@users.noreply.github.com>
2021-08-17 15:17:23 +0900

    Add Fixed Data

---
+++ Assets/Scripts/FIxedValue.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class FIxedValue
+{
+    public static string SAVE_PATH = $"{Application.persistentDataPath}/{"gamedata.save"}";
+    public static int MAX_LEVEL = 4;
+}
